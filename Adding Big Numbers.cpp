a912d91134a25f1e7cc5e398861462c433c92af9
Grigorev Grigorii <55593191+GrigorevGrigorii@users.noreply.github.com>
2020-03-12 21:45:30 +0300

    Create  Adding Big Numbers.cpp

---
+++ Adding Big Numbers.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+std::string add(const std::string& a, const std::string& b) {
+  std::string sumstr = "";
+    std::string c = a, d = b;
+    while (c.size() != d.size()) {
+        if (c.size() < d.size()) c = "0" + c;
+        else d = '0' + d;
+    }
+    int p = 0;
+    int sum = 0;
+    for (int i = 1; i <= c.size(); ++i) {
+        sum = int(c[c.size()-i]) - 48 + int(d[d.size()-i]) - 48 + p;
+        p = sum / 10;
+        sumstr = char(char(sum % 10) + 48) + sumstr;
+    }
+    if(p!=0)sumstr = char(char(p)+48)+sumstr;
+    return sumstr;
+}
